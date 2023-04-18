7258b920f273fb4e296e80396067c837706df0f9
Daniel Laug <daniel.laugvz@gmail.com>
2023-04-18 17:31:23 -0600

    CheckBox Terminos y condiciones (No me deja mostrar las imágenes)

--- AppMedicina/TermsConditionsViewController.swift
+++ AppMedicina/TermsConditionsViewController.swift
@@ -13,24 +13,33 @@ class TermsConditionsViewController: UIViewController {
     @IBOutlet var termsCondiTitle: UITextView!
     
     @IBOutlet var textTermsCondi: UITextView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var btnCheckBox:UIButton!
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           // Do any additional setup after loading the view, typically from a nib.
+              btnCheckBox.setImage(UIImage(named:"Checkmarkempty"), for: .normal)
+              btnCheckBox.setImage(UIImage(named:"Checkmark"), for: .selected)
+       }
+    @IBAction func TermsAction(_ sender: UIButton) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBOutlet weak var TermsOutlet: UIButton!
+    
+       
+        //MARK:- checkMarkTapped
+    @IBAction func checkMarkTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+                    sender.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+                    
+                }) { (success) in
+                    UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+                        sender.isSelected = !sender.isSelected
+                        sender.transform = .identity
+                    }, completion: nil)
+                }
     }
-    */
-
     
     @IBAction func nextTermsButton(_ sender: UIButton) {
     }
