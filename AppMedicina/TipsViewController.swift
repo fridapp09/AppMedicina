40be6490057109512afffda80c43469edd2e46f8
Frida <frida_perez@hotmail.com>
2023-04-19 16:54:04 -0600

    scroll view en tips

--- AppMedicina/TipsViewController.swift
+++ AppMedicina/TipsViewController.swift
@@ -9,6 +9,23 @@ import UIKit
 
 class TipsViewController: UIViewController {
 
+    @IBOutlet var tipsTitle: UITextView!
+    
+    @IBOutlet var tip1Title: UITextView!
+    @IBOutlet var tip1Text: UITextView!
+    
+    @IBOutlet var tip2Title: UITextView!
+    @IBOutlet var tip2Text: UITextView!
+    
+    @IBOutlet var tip3Title: UITextView!
+    @IBOutlet var tip3Text: UITextView!
+    
+    @IBOutlet var tip4Title: UITextView!
+    @IBOutlet var tip4Text: UITextView!
+    
+    @IBOutlet var tip5Title: UITextView!
+    @IBOutlet var tip5Text: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -27,5 +44,13 @@ class TipsViewController: UIViewController {
     */
 
     @IBAction func tips1Button(_ sender: UIButton) {
+    }
+    @IBAction func tips2Button(_ sender: UIButton) {
+    }
+    @IBAction func tips3Button(_ sender: UIButton) {
+    }
+    @IBAction func tips4Button(_ sender: Any) {
+    }
+    @IBAction func tips5Button(_ sender: Any) {
     }
 }
