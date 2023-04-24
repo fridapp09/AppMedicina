29a706eb7704f51ee2d7424e8f78c45b0ea74b8b
Daniel Laug <daniel.laugvz@gmail.com>
2023-04-24 13:24:29 -0600

    Checkbox funcional de términos y condiciones

--- AppMedicina/TermsConditionsViewController.swift
+++ AppMedicina/TermsConditionsViewController.swift
@@ -14,32 +14,25 @@ class TermsConditionsViewController: UIViewController {
     
     @IBOutlet var textTermsCondi: UITextView!
 
-   /* @IBOutlet weak var btnCheckBox:UIButton!*/
-       
+    @IBOutlet weak var checkbox1: UIButton!
+    
+    var flag1 = false
+    
        override func viewDidLoad() {
            super.viewDidLoad()
-           /*Do any additional setup after loading the view, typically from a nib.
-              //btnCheckBox.setImage(UIImage(named:"Checkmarkempty"), for: .normal)
-              //btnCheckBox.setImage(UIImage(named:"Checkmark"), for: .selected)
+          
        }
-    @IBAction func TermsAction(_ sender: UIButton) {
+
+    @IBAction func checkboxtn1(_ sender: UIButton) {
+        if (flag1 == false){
+            sender.backgroundColor = UIColor.green
+            flag1 = true
+        }
+        else{
+            sender.backgroundColor = UIColor.red
+            flag1 = false
+        }
     }
-    
-    @IBOutlet weak var TermsOutlet: UIButton!
-    
-       
-        //MARK:- checkMarkTapped
-    @IBAction func checkMarkTapped(_ sender: UIButton) {
-        UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
-                    sender.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
-                    
-                }) { (success) in
-                    UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
-                        sender.isSelected = !sender.isSelected
-                        sender.transform = .identity
-                    }, completion: nil)
-                }
-    */}
     
     @IBAction func nextTermsButton(_ sender: UIButton) {
     }
