af6e8b1e6c5f53744e8b21afde3117faa20c5399
FridaPerez <frida_perez@hotmail.com>
2023-05-02 00:38:42 -0600

    cambios info meds

---
+++ AppMedicina/Medicines/Information Medicines/Med2Dis4ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Med2Dis4ViewController.swift
+//  AppMedicina
+//
+//  Created by Frida Pérez on 02/05/23.
+//
+
+import UIKit
+
+class Med2Dis4ViewController: UIViewController {
+    
+    @IBOutlet weak var med2Title: UITextView!
+            
+    @IBOutlet weak var med2Imagen: UIImageView!
+            
+    @IBOutlet weak var med2Text: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ AppMedicina/Medicines/Information Medicines/Med2Dis6ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  Med2Dis6ViewController.swift
+//  AppMedicina
+//
+//  Created by Frida Pérez on 02/05/23.
+//
+
+import UIKit
+
+class Med2Dis6ViewController: UIViewController {
+
+    @IBOutlet weak var med2Title: UITextView!
+    
+    @IBOutlet weak var med2Imagen: UIImageView!
+    
+    @IBOutlet weak var med2Text: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
